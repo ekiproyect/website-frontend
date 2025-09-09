73b6c833a1ba64c1c078b28aad46463f3c69ebd5
Renato Morales <104334139+naatox@users.noreply.github.com>
2025-09-09 12:38:51 -0300

    Add build configuration for rollup options in Vite

--- vite.config.ts
+++ vite.config.ts
@@ -19,4 +19,12 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        notfound: "index.html",
+      },
+    },
+  },
 }));
