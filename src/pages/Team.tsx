792f2872aab915e31fca2fbc31235fecabc40442
Renato Morales <104334139+naatox@users.noreply.github.com>
2025-11-26 16:39:42 -0300

    Update email address for team member in social links

--- src/pages/Team.tsx
+++ src/pages/Team.tsx
@@ -86,7 +86,7 @@ const Team = () => {
         "Backups & DR",
         "Cost Control",
       ],
-      social: { github: "https://github.com/naatox", linkedin: "https://www.linkedin.com/in/renato-morales-constancio/", email: "ana@eki.com" },
+      social: { github: "https://github.com/naatox", linkedin: "https://www.linkedin.com/in/renato-morales-constancio/", email: "rmoralesconstancio@gmail.com" },
     },
   ];
 
